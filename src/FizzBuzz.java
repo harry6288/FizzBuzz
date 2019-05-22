85990824f2e9333a94e24a2de8b5c7afab0cbe54
hardeepkaur <hardeepkaur@10.51.204.228>
2019-05-22 12:24:56 -0400

    r3 green

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,11 @@
 
 public class FizzBuzz {
 	public String buzz(int num) {
-		if(num %5 == 0) {
+		if(num %15 == 0) {
+			return "fizzbuzz";
+		}
+		else if(num % 5 == 0)
+		{
 			return "buzz";
 		}
 		return "fizz";
